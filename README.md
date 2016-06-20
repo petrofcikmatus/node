61a2c320dddaf391d31f9acb6b15ef26953cc49c
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-06-20 22:20:20 +0200

    another update :)

---
+++ README.md
@@ -0,0 +1,39 @@
+# Node.js learning app
+
+## Uses
+
+```
+npm install express --save
+npm install express-handlebars --save
+npm install knex --save
+npm install nodemon --save
+```
+
+## Run
+
+```
+nodemon app.js
+```
+
+Or
+
+```
+node app.js
+```
+
+## Notes
+
+- https://www.paralelnipolis.cz/
+- call stack, najprv synchronne funkcie a potom skočí na asynchronne.
+- node app.js
+- http://expressjs.com/
+- http://knexjs.org/
+- je kompilovaný, pri spustení sa skompiluje a je to
+- 1 process carry about everything, must catch errors, memory leaks...
+- https://github.com/ericf/express-handlebars (or underscore, jade)
+- http://nodemon.io/
+- nvm - node version manager
+- https://github.com/ParalelniPolis
+- https://github.com/petrofcikmatus/rfid-access-system-api
+- selenium v nodejs, http://webdriver.io
+- https://mochajs.org/ test framework for node
--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// pridanie serveru express
 // @link: http://expressjs.com/
 var express = require('express');
 
+// pridanie template enginu handlebars
 // @link: https://github.com/ericf/express-handlebars
 var express_handlebars = require('express-handlebars');
 
+// pridanie a nastavenie knex databázového wrapperu
 // @link: http://knexjs.org/
 var knex = require('knex')({
     client: 'mysql',
@@ -15,16 +18,24 @@ var knex = require('knex')({
     }
 });
 
+// vytvorenie express inštancie
 var app = express();
 
+// pridanie handlebars ako view enginu pre express
 app.engine('handlebars', express_handlebars({defaultLayout: 'main'}));
+
+// nastavenie defaultného view enginu na handlebars
 app.set('view engine', 'handlebars');
 
+// pridanie middleware, vždy sa vykoná predtým než sa vykoná nasledujúce
 app.use(function (req, res, next) {
     console.log(req.url);
+
+    // spustí vykonanie nasledujúceho
     next();
 });
 
+// routa na /, teda hlavnú stránku
 app.get('/', function (req, res) {
     knex.select('*').from('books').then(function (results) {
         res.render('home', {
@@ -36,6 +47,22 @@ app.get('/', function (req, res) {
     });
 });
 
+// routa na book/:id
+app.get('/book/:id', function (req, res) {
+    var id = req.params.id;
+
+    knex.select('*').from('books').where('id', id).then(function (results) {
+        res.render('book', {
+            books: results
+        });
+        //res.json(results);
+    }).catch(function (error) {
+        console.log(error);
+    });
+});
+
+
+// routa na book/:id/delete
 app.get('/book/:id/delete', function (req, res) {
     var id = req.params.id;
 
@@ -46,26 +73,15 @@ app.get('/book/:id/delete', function (req, res) {
     });
 });
 
-app.get('/book/:id', function (req, res) {
-    var id = req.params.id;
-
-    knex.select('*').from('books').where('id', id).then(function (results) {
-        res.render('home', {
-            books: results
-        });
-        //res.json(results);
-    }).catch(function (error) {
-        console.log(error);
-    });
-});
-
+// všetky predtým nehachytené routy, vhodné pre 404
 app.get('*', function (req, res) {
     res.json(req.url);
 });
 
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000!');
+// spustenie serveru na danom porte
+var port = 8000;
+app.listen(port, function () {
+    console.log('App is listening on port ' + port + '!');
 });
 
-
-
+// v konzoli zapnúť pomocou node 'app.js', alebo 'nodemon app.js'
