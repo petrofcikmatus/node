aa026407983c48d8df3a1a19a7dc888b72851d13
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-06-20 18:50:07 +0200

    update

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+// @link: http://expressjs.com/
+var express = require('express');
+var app = express();
 
+// @link: http://knexjs.org/
 var knex = require('knex')({
     client: 'mysql',
     connection: {
-        host     : '127.0.0.1',
-        user     : 'root',
-        password : 'root',
-        database : 'node'
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'node'
     }
 });
 
-knex.select().from('books').then(function (results) {
-    
-    })
-    .catch(function (error) {
-
+app.get('/', function (request, response) {
+    knex.select('*').from('books').then(function (results) {
+        response.json(results);
+    }).catch(function (error) {
+        console.log(error);
     });
+});
+
+app.listen(8000, function () {
+    console.log('Ecample app listening on port 8000!');
+});
+
+
+
