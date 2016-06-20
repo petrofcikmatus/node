aad7323fbe3f14f3f83f7405014b13867df25a80
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-06-20 18:25:58 +0200

    update

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host     : '127.0.0.1',
+        user     : 'root',
+        password : 'root',
+        database : 'node'
+    }
+});
 
+knex.select().from('books').then(function (results) {
+    
+    })
+    .catch(function (error) {
+
+    });
